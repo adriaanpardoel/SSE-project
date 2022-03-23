9bafb9af7f1a0539c50297dd9a639966ca23180e
Dyon <dyonx@hotmail.com>
2022-03-23 14:40:46 +0100

    remove parameters

--- measurements/script.cmd
+++ measurements/script.cmd
@@ -4,13 +4,13 @@ timeout 5
 
 :: hidden layer size: 100, 200, 500
 :: features: 20, 50, 100
-:: samples: 10000, 50000, 100000
+:: samples: 1000, 5000, 10000
 :: classes: 2, 3, 7
 :: layers: 1, 2, 3
 
 for %%u in (20, 50, 100) do (
 	for %%v in (2, 3, 7) do (
-		for %%w in (10000, 20000, 50000) do (
+		for %%w in (1000, 2000, 5000) do (
 			for %%z in (100, 200, 500) do (
 				"%PROGRAMFILES%"\Intel\"Power Gadget 3.6"\IntelPowerGadget.exe -start
 				python train_network.py --features %%u --classes %%v --samples %%w --hidden-layer-sizes %%z --log-file result.csv
@@ -23,7 +23,7 @@ for %%u in (20, 50, 100) do (
 
 for %%u in (20, 50, 100) do (
 	for %%v in (2, 3, 7) do (
-		for %%w in (10000, 20000, 50000) do (
+		for %%w in (1000, 2000, 5000) do (
 			for %%z in (100, 200, 500) do (
 				"%PROGRAMFILES%"\Intel\"Power Gadget 3.6"\IntelPowerGadget.exe -start
 				python train_network.py --features %%u --classes %%v --samples %%w --hidden-layer-sizes %%z %%z --log-file result.csv
@@ -36,7 +36,7 @@ for %%u in (20, 50, 100) do (
 
 for %%u in (20, 50, 100) do (
 	for %%v in (2, 3, 7) do (
-		for %%w in (10000, 20000, 50000) do (
+		for %%w in (1000, 2000, 5000) do (
 			for %%z in (100, 200, 500) do (
 				"%PROGRAMFILES%"\Intel\"Power Gadget 3.6"\IntelPowerGadget.exe -start
 				python train_network.py --features %%u --classes %%v --samples %%w --hidden-layer-sizes %%z %%z %%z --log-file result.csv
